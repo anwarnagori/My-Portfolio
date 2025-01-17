902a8e9a15ff21e815ea542ec96199b9e9a69fa0
anwarnagori <ar1498137@gmail.com>
2025-01-17 18:12:51 +0500

    updated

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import './style.scss'
 
 const Home = () => {
   return (
-    <div>
-      Home page
-    </div>
+    <section id='home' className='home'>
+       <div className='home__text-wrapper'>
+        <h1>
+          Hello, I'm Muhammad Anwer
+          <br />
+          Front end developer
+        </h1>
+       </div>
+    </section>
   )
 }
 
